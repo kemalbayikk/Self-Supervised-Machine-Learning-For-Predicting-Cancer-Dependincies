ca6addcdd6e9f45dc0947e9db9b42398b90b7460
Kemal Bayik <kb611@m902175.pws.inf.susx.ac.uk>
2024-07-31 19:04:33 +0100

    VAE DeepDep 025 Beta TCGA Training done, modality dropout with fprint done

--- PytorchStaticSplits/DeepDepVAE/Modality_dropout/DeepDepModalityDropoutVAE.py
+++ PytorchStaticSplits/DeepDepVAE/Modality_dropout/DeepDepModalityDropoutVAE.py
@@ -123,8 +123,8 @@ def train_model(model, train_loader, val_loader, num_epoch, patience, learning_r
             optimizer.zero_grad()
             
             # Dropout maskesi oluştur
-            drop_mask = [torch.rand(1).item() < p_drop for _ in range(4)] + [False]
-            # drop_mask = [torch.rand(1).item() < p_drop for _ in range(5)]
+            # drop_mask = [torch.rand(1).item() < p_drop for _ in range(4)] + [False]
+            drop_mask = [torch.rand(1).item() < p_drop for _ in range(5)]
             # drop_mask = [0,0,0,0,1]
 
             # Drop maskesi bilgisini yazdır
@@ -181,7 +181,7 @@ def train_model(model, train_loader, val_loader, num_epoch, patience, learning_r
             best_loss = val_loss
             epochs_no_improve = 0
             best_model_state_dict = model.state_dict()
-            torch.save(best_model_state_dict, 'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/best_model_vae_modality_dropout_newVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.pth')
+            torch.save(best_model_state_dict, 'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/best_model_vae_modality_dropout_newVAE_withsplit3_LAST_AfterDecision.pth')
             print("Model saved")
 
     return best_model_state_dict, training_predictions, training_targets_list
@@ -265,7 +265,7 @@ if __name__ == '__main__':
     with open(f'Data/data_split_{split_num}.pickle', 'rb') as f:
             train_dataset, val_dataset, test_dataset = pickle.load(f)
 
-    wandb.init(project="Latest-Model-VAE-PredictionNetwork", entity="kemal-bayik", name=f"Prediction_Network_Modality_Dropout_VAE_withsplit{split_num}_NoDropoutFprintTrainingTime")
+    wandb.init(project="Latest-Model-VAE-PredictionNetwork", entity="kemal-bayik", name=f"Prediction_Network_Modality_Dropout_VAE_withsplit{split_num}_All_Modalities")
 
     config = wandb.config
     config.learning_rate = 1e-4
@@ -307,11 +307,11 @@ if __name__ == '__main__':
         })
         
         # Tahmin ve gerçek değerleri kaydetme
-        np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_test_mask_{mask}_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', res['targets'], fmt='%.6f')
-        np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_test_mask_{mask}_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', res['predictions'], fmt='%.6f')
+        np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_test_mask_{mask}_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', res['targets'], fmt='%.6f')
+        np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_test_mask_{mask}_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', res['predictions'], fmt='%.6f')
 
     # Save the best model
-    torch.save(best_model_state_dict, 'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/deepdep_vae_model_modality_dropout_NewVAE_split3_NoDropoutFprintTrainingTime_LAST_AfterDecision.pth')
+    torch.save(best_model_state_dict, 'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/deepdep_vae_model_modality_dropout_NewVAE_split3_LAST_AfterDecision.pth')
 
     # Plot results
     y_true_train = np.array(training_targets_list).flatten()
@@ -319,10 +319,10 @@ if __name__ == '__main__':
     y_true_test = results["0_0_0_0_0"]["targets"].flatten()  # Hiçbir modalite kapalı değilken
     y_pred_test = results["0_0_0_0_0"]["predictions"].flatten()  # Hiçbir modalite kapalı değilken
 
-    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_train_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', y_true_train, fmt='%.6f')
-    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_train_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', y_pred_train, fmt='%.6f')
-    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_test_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', y_true_test, fmt='%.6f')
-    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_test_VAE_Modality_Dropout_NewVAE_withsplit3_NoDropoutFprintTrainingTime_LAST_AfterDecision.txt', y_pred_test, fmt='%.6f')
+    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_train_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', y_true_train, fmt='%.6f')
+    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_train_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', y_pred_train, fmt='%.6f')
+    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_true_test_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', y_true_test, fmt='%.6f')
+    np.savetxt(f'PytorchStaticSplits/DeepDepVAE/Results/Split3/ModalityDropout/Predictions/y_pred_test_VAE_Modality_Dropout_NewVAE_withsplit3_LAST_AfterDecision.txt', y_pred_test, fmt='%.6f')
 
     print(f"Training: y_true_train size: {len(y_true_train)}, y_pred_train size: {len(y_pred_train)}")
     print(f"Testing: y_true_test size: {len(y_true_test)}, y_pred_test size: {len(y_pred_test)}")
